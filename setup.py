26b6d9503685966cf424321a00536da3076cb568
Emily Dolson <EmilyLDolson@gmail.com>
2019-03-01 12:25:28 -0500

    Add test infrastructure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='avida_converters',
+      version='0.1',
+      description='Converting avida data to standards format',
+      url='https://github.com/alife-data-standards/converters-avida',
+      author='Alex Lalejini',
+      author_email='lalejini@msu.edu',
+      license='MIT',
+      packages=['avida_converters'],
+      zip_safe=False)
---
+++ tests/test_phylogeny.py
@@ -0,0 +1,11 @@
+import avida_converters as ac
+import pandas as pd
+import json
+
+def test_Convert_AvidaSpop_To_stdPhylogeny_Sexual():
+    ac.Convert_AvidaSpop_To_StdPhylogeny("example_data/example-avida-sexual.spop", 
+                                         "sexual_phylogeny_test.csv")
+    data = pd.read_csv("sexual_phylogeny_test.csv", index_col="id")
+
+    assert json.loads(data.loc[1524187, "ancestor_list"]) == [1520248,1523080]
+    assert data.loc[1524187, "merit"] == 200704
