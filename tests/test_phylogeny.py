3d78e936adac338ce27edb9906b912b7261855c8
Alex Lalejini <amlalejini@gmail.com>
2019-03-02 10:40:26 -0500

    added test for invalid output format for phylogeny conveter

--- tests/test_phylogeny.py
+++ tests/test_phylogeny.py
@@ -15,3 +15,10 @@ def test_Convert_AvidaSpop_To_StdPhylogeny_InvalidInputPath():
     assert not os.path.isfile(input_fpath), "{} needs to not exist for this test to pass".format(input_fpath)
     with pytest.raises(ValueError):
         ac.Convert_AvidaSpop_To_StdPhylogeny(input_fpath)
+
+def test_Convert_AvidaSpop_To_StdPhylogeny_InvalidOutputFormat():
+    output_format = "this_should_never_be_a_valid_format_ever"
+    assert output_format not in ac.VALID_OUT_FORMATS, "{} should not be a valid output format for this test to pass".format(output_format)
+    with pytest.raises(ValueError):
+        ac.Convert_AvidaSpop_To_StdPhylogeny("example_data/example-avida-sexual.spop",
+                                             output_format=output_format)
