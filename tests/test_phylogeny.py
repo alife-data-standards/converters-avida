0e75b4d930a83ca114aa393374f213a039e2f898
Alex Lalejini <amlalejini@gmail.com>
2019-03-02 10:16:03 -0500

    add test for no input file for phylogeny converter

--- tests/test_phylogeny.py
+++ tests/test_phylogeny.py
@@ -1,6 +1,6 @@
 import avida_converters as ac
 import pandas as pd
-import json
+import json, pytest, os
 
 def test_Convert_AvidaSpop_To_stdPhylogeny_Sexual():
     ac.Convert_AvidaSpop_To_StdPhylogeny("example_data/example-avida-sexual.spop",
@@ -9,3 +9,9 @@ def test_Convert_AvidaSpop_To_stdPhylogeny_Sexual():
 
     assert json.loads(data.loc[1524187, "ancestor_list"]) == [1520248,1523080]
     assert data.loc[1524187, "merit"] == 200704
+
+def test_Convert_AvidaSpop_To_StdPhylogeny_InvalidInputPath():
+    input_fpath = "this_file_does_not_actually_exist.spop"
+    assert not os.path.isfile(input_fpath), "{} needs to not exist for this test to pass".format(input_fpath)
+    with pytest.raises(ValueError):
+        ac.Convert_AvidaSpop_To_StdPhylogeny(input_fpath)
