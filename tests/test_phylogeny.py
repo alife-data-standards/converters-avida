2f969c178c18dd9cc1bc5900b301c5de9007ace7
Emily Dolson <EmilyLDolson@gmail.com>
2019-03-04 14:25:06 -0500

    Fix use of json.loads

--- tests/test_phylogeny.py
+++ tests/test_phylogeny.py
@@ -1,13 +1,15 @@
 import avida_converters as ac
 import pandas as pd
-import json, pytest, os
+import ast
+import pytest
+import os
 
 def test_Convert_AvidaSpop_To_stdPhylogeny_Sexual():
     ac.Convert_AvidaSpop_To_StdPhylogeny("example_data/example-avida-sexual.spop",
                                          "sexual_phylogeny_test.csv")
     data = pd.read_csv("sexual_phylogeny_test.csv", index_col="id")
 
-    assert json.loads(data.loc[1524187, "ancestor_list"]) == [1520248,1523080]
+    assert ast.literal_eval(data.loc[1524187, "ancestor_list"]) == [1520248,1523080]
     assert data.loc[1524187, "merit"] == 200704
 
 def test_Convert_AvidaSpop_To_StdPhylogeny_InvalidInputPath():
